cf3b07b2e8b5d54461982f10c062242406e2942f
Matthias Kruk <m@m10k.eu>
2021-03-13 23:08:57 +0900

    config: Add setting for the width of tabs
    This commit adds a setting to the config structure that allows the width
    of tabs to be adjusted dynamically.

--- config.c
+++ config.c
@@ -1,5 +1,6 @@
 #include "config.h"
 
 struct config config = {
-	.file_default_mode = CONFIG_FILE_DEFAULT_MODE
+	.file_default_mode = CONFIG_FILE_DEFAULT_MODE,
+	.tab_width = CONFIG_DEFAULT_TAB_WIDTH
 };
--- config.h
+++ config.h
@@ -2,9 +2,11 @@
 #define CONFIG_H
 
 #define CONFIG_FILE_DEFAULT_MODE 0600
+#define CONFIG_DEFAULT_TAB_WIDTH 8
 
 struct config {
 	int file_default_mode;
+	int tab_width;
 };
 
 #ifndef __E_CONFIG
